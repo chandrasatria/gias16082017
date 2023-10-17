5ca00bfa88262837c8d523c3d24c4011d74452ff
chandrasatria <chandra.invi@gmail.com>
2023-10-17 18:27:54 +0700

    Update patch_ste.py

--- addons/patch_ste.py
+++ addons/patch_ste.py
@@ -185,7 +185,7 @@ def start_repair():
         SELECT sle.voucher_type,sle.`voucher_no`
         FROM `tabStock Ledger Entry` sle
        
-        WHERE sle.posting_date = "2022-08-18" and sle.is_cancelled = 0
+        WHERE sle.posting_date = "2022-08-27" and sle.is_cancelled = 0
         GROUP BY sle.`voucher_no` 
         ORDER BY sle.`posting_date`
 
