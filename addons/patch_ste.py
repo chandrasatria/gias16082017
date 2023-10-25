f183ebec92de1c2c388eb05a1bdb9f5b033da15d
chandrasatria <chandra.invi@gmail.com>
2023-10-25 14:37:52 +0700

    Update patch_ste.py

--- addons/patch_ste.py
+++ addons/patch_ste.py
@@ -308,7 +308,7 @@ def repair_gl_entry_untuk_ste_debug_4():
 def repair_gl_entry_untuk_ste_debug():
     list_dn = frappe.db.sql(""" 
 
-       SELECT "STER-BLI-1-23-09-00010"
+       SELECT "STEI-HO-22-08-01616"
          """)
     for row in list_dn:
         repair_gl_entry_untuk_ste("Stock Entry",row[0])
